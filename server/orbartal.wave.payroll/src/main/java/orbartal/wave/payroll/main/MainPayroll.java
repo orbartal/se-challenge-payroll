7e1fe3a1de05b128c1215ae99d434163312a202e
orbartal <orbartal@gmail.com>
2020-05-24 17:00:42 -0400

    First code commit. Init maven and java spring boot server.

---
+++ server/orbartal.wave.payroll/src/main/java/orbartal/wave/payroll/main/MainPayroll.java
@@ -0,0 +1,12 @@
+package orbartal.wave.payroll.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainPayroll {
+	public static void main(String[] args) {
+		SpringApplication.run(MainPayroll.class, args); 
+	}
+}
+
